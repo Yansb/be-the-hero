a81638c76bef41020293f05250fe93a66048c762
Yan Santana <yansbarreiro@gmail.com>
2020-03-27 03:08:06 -0300

    mobile da aplicaçõ

---
+++ mobile/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import Route from './src/routes';
+
+export default function App() {
+  return (
+    <Route/>
+  );
+}
